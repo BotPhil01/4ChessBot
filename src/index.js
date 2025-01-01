643997e00682e6e11d9d347e9bbbf0338844ee19
BotPhil01 <acsrobles39@gmail.com>
2025-01-01 00:24:27 +0000

    Feat: draggable div implemented

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ag_grid_community_1 = require("ag-grid-community");
 ag_grid_community_1.ModuleRegistry.registerModules([ag_grid_community_1.ClientSideRowModelModule]);
 window.onload = function () {
+    console.log("starting up");
     buttons();
 };
 function buttons() {
@@ -15,42 +16,49 @@ function buttons() {
         bb.onclick = swap;
     }
     var db = document.getElementById("debugButton");
-    if (db) {
-        db.onclick = f;
+    if (document.getElementById("draggable")) {
+        console.log("dragging element");
+        dragElement("draggable");
     }
+    // if (db) {db.onclick = f;}
 }
-function f() {
-    const gridOptions = {
-        rowData: [{ make: "Tesla", model: "Model Y", price: 64950, electric: true }],
-        columnDefs: [
-            { field: "make" },
-            { field: "model" },
-            { field: "price" },
-            { field: "electric" },
-        ],
-        defaultColDef: {
-            flex: 1,
-        },
-        getRowId: (params) => {
-            return params.data.make + params.data.model;
-        },
-        onRowSelected: (event) => {
-            if (event.data) {
-                const price = event.data.price;
-            }
-        }
-    };
-    const gridElem = document.getElementById("pieceGrid");
-    if (!gridElem) {
-        console.error("no element found");
-        return;
-    }
-    else {
-        console.log("creating grid with options");
-        console.log(`${gridOptions}`);
-        (0, ag_grid_community_1.createGrid)(gridElem, gridOptions);
-    }
-}
+// function f() {
+//     interface ICar {
+//         make: string,
+//         model: string,
+//         price: number,
+//         electric: boolean
+//     }
+//     const gridOptions: GridOptions<ICar> = {
+//         rowData: [{ make: "Tesla", model: "Model Y", price: 64950, electric: true }],
+//         columnDefs: [
+//             {field: "make"},
+//             {field: "model"},
+//             {field: "price"},
+//             {field: "electric"},
+//         ],
+//         defaultColDef: {
+//             flex: 1,
+//         },
+//         getRowId: (params) => {
+//             return params.data.make + params.data.model;
+//         },
+//         onRowSelected: (event) => {
+//             if (event.data) {
+//                 const price = event.data.price;
+//             }
+//         }
+//     };
+//     const gridElem = document.getElementById("pieceGrid");
+//     if (!gridElem) {
+//         console.error("no element found");
+//         return;
+//     } else {
+//         console.log("creating grid with options");
+//         console.log(`${gridOptions}`);
+//         createGrid(gridElem, gridOptions);
+//     }
+// }
 function swap() {
     console.log("swapping");
     let gameDiv = document.getElementById("gameDiv");
@@ -67,5 +75,44 @@ function swap() {
     else {
         homeDiv.setAttribute("class", "centered");
         gameDiv.setAttribute("class", "destroy");
+    }
+}
+function dragElement(elementId) {
+    var pos1 = 0;
+    var pos2 = 0;
+    var pos3 = 0;
+    var pos4 = 0;
+    var element = document.getElementById(elementId);
+    if (!element) {
+        console.error(`Unable to retrieve element <${elementId}>`);
+        return;
+    }
+    element.onmousedown = dragMouseDown;
+    function dragMouseDown(e) {
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+        return true;
+    }
+    function elementDrag(ev) {
+        ev.preventDefault();
+        pos1 = pos3 - ev.clientX;
+        pos2 = pos4 - ev.clientY;
+        pos3 = ev.clientX;
+        pos4 = ev.clientY;
+        if (element) {
+            element.style.top = (element.offsetTop - pos2) + "px";
+            element.style.left = (element.offsetLeft - pos1) + "px";
+            return true;
+        }
+        console.error("Element has been clicked dragged and disappeared");
+        return false;
+    }
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+        return true;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import {createGrid, GridOptions, ClientSideRowModelModule, ModuleRegistry} from 'ag-grid-community';
 
 ModuleRegistry.registerModules([ ClientSideRowModelModule ]);
+
+
+
 window.onload = function() {
+    console.log("starting up");
     buttons();
 }
 
@@ -11,58 +15,62 @@ function buttons() {
     var bb = document.getElementById("boardBackButton")
     if (bb) {bb.onclick = swap;}
     var db = document.getElementById("debugButton")
-    if (db) {db.onclick = f;}
+    if (document.getElementById("draggable")) {
+        console.log("dragging element");
+        dragElement("draggable");
+    }
+    // if (db) {db.onclick = f;}
 }
 
-function f() {
+// function f() {
 
-    interface ICar {
-        make: string,
-        model: string,
-        price: number,
-        electric: boolean
-    }
+//     interface ICar {
+//         make: string,
+//         model: string,
+//         price: number,
+//         electric: boolean
+//     }
 
 
 
-    const gridOptions: GridOptions<ICar> = {
-        rowData: [{ make: "Tesla", model: "Model Y", price: 64950, electric: true }],
+//     const gridOptions: GridOptions<ICar> = {
+//         rowData: [{ make: "Tesla", model: "Model Y", price: 64950, electric: true }],
 
-        columnDefs: [
-            {field: "make"},
-            {field: "model"},
-            {field: "price"},
-            {field: "electric"},
-        ],
+//         columnDefs: [
+//             {field: "make"},
+//             {field: "model"},
+//             {field: "price"},
+//             {field: "electric"},
+//         ],
 
-        defaultColDef: {
-            flex: 1,
-        },
+//         defaultColDef: {
+//             flex: 1,
+//         },
 
-        getRowId: (params) => {
-            return params.data.make + params.data.model;
-        },
+//         getRowId: (params) => {
+//             return params.data.make + params.data.model;
+//         },
 
-        onRowSelected: (event) => {
-            if (event.data) {
-                const price = event.data.price;
-            }
-        }
-    };
+//         onRowSelected: (event) => {
+//             if (event.data) {
+//                 const price = event.data.price;
+//             }
+//         }
+//     };
 
-    const gridElem = document.getElementById("pieceGrid");
+//     const gridElem = document.getElementById("pieceGrid");
 
-    if (!gridElem) {
-        console.error("no element found");
-        return;
-    } else {
-        console.log("creating grid with options");
-        console.log(`${gridOptions}`);
-        createGrid(gridElem, gridOptions);
-    }
+//     if (!gridElem) {
+//         console.error("no element found");
+//         return;
+//     } else {
+//         console.log("creating grid with options");
+//         console.log(`${gridOptions}`);
+//         createGrid(gridElem, gridOptions);
+//     }
     
 
-}
+// }
 
 function swap() {
     console.log("swapping");
@@ -83,3 +91,51 @@ function swap() {
     }
 }
 
+function dragElement(elementId: string) {
+    var pos1 = 0; 
+    var pos2 = 0;
+    var pos3 = 0;
+    var pos4 = 0;
+
+    var element = document.getElementById(elementId);
+    if (!element) {
+        console.error(`Unable to retrieve element <${elementId}>`);
+        return;
+    }
+    element.onmousedown = dragMouseDown;
+
+
+    function dragMouseDown(e: MouseEvent) {
+        e.preventDefault();
+        pos3 = e.clientX
+        pos4 = e.clientY
+        document.onmouseup = closeDragElement;
+        
+        document.onmousemove = elementDrag;
+        return true;
+    }
+
+    function elementDrag(ev: MouseEvent) {
+        ev.preventDefault();
+
+        pos1 = pos3 - ev.clientX;
+        pos2 = pos4 - ev.clientY;
+        pos3 = ev.clientX;
+        pos4 = ev.clientY;
+        
+        if (element) {
+            element.style.top = (element.offsetTop - pos2) + "px";
+            element.style.left = (element.offsetLeft - pos1) + "px";
+            return true;
+        }
+        console.error("Element has been clicked dragged and disappeared");
+        return false;
+        
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+        return true;
+    }
+}
